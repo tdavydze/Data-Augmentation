b685849ba2863f6b3e2358ef24c6b77afd256b06
Tsimur Davydzenka <53912571+tdavydze@users.noreply.github.com>
2021-11-22 21:20:45 -0700

    Add files via upload

---
+++ UCMerced/ResNet-50.py
@@ -0,0 +1,139 @@
+import numpy as np 
+from tensorflow.keras.layers import Conv2D, Dense, Flatten
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.normalization import BatchNormalization
+from keras.layers import Input
+from keras.models import Model
+from keras.layers import  Add, Activation, ZeroPadding2D, AveragePooling2D
+from keras.initializers import glorot_uniform
+
+
+def identity_block(X, f, filters, stage, block):
+   
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+    F1, F2, F3 = filters
+
+    X_shortcut = X
+   
+    X = Conv2D(filters=F1, kernel_size=(1, 1), strides=(1, 1), padding='valid', name=conv_name_base + '2a', kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name=bn_name_base + '2a')(X)
+    X = Activation('relu')(X)
+
+    X = Conv2D(filters=F2, kernel_size=(f, f), strides=(1, 1), padding='same', name=conv_name_base + '2b', kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name=bn_name_base + '2b')(X)
+    X = Activation('relu')(X)
+
+    X = Conv2D(filters=F3, kernel_size=(1, 1), strides=(1, 1), padding='valid', name=conv_name_base + '2c', kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name=bn_name_base + '2c')(X)
+
+    X = Add()([X, X_shortcut])# SKIP Connection
+    X = Activation('relu')(X)
+
+    return X
+
+
+def convolutional_block(X, f, filters, stage, block, s=2):
+   
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    F1, F2, F3 = filters
+
+    X_shortcut = X
+
+    X = Conv2D(filters=F1, kernel_size=(1, 1), strides=(s, s), padding='valid', name=conv_name_base + '2a', kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name=bn_name_base + '2a')(X)
+    X = Activation('relu')(X)
+
+    X = Conv2D(filters=F2, kernel_size=(f, f), strides=(1, 1), padding='same', name=conv_name_base + '2b', kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name=bn_name_base + '2b')(X)
+    X = Activation('relu')(X)
+
+    X = Conv2D(filters=F3, kernel_size=(1, 1), strides=(1, 1), padding='valid', name=conv_name_base + '2c', kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name=bn_name_base + '2c')(X)
+
+    X_shortcut = Conv2D(filters=F3, kernel_size=(1, 1), strides=(s, s), padding='valid', name=conv_name_base + '1', kernel_initializer=glorot_uniform(seed=0))(X_shortcut)
+    X_shortcut = BatchNormalization(axis=3, name=bn_name_base + '1')(X_shortcut)
+
+    X = Add()([X, X_shortcut])
+    X = Activation('relu')(X)
+
+    return X
+
+
+def ResNet50(input_shape=(256, 256, 3)):
+
+    X_input = Input(input_shape)
+
+    X = ZeroPadding2D((3, 3))(X_input)
+
+    X = Conv2D(64, (7, 7), strides=(2, 2), name='conv1', kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name='bn_conv1')(X)
+    X = Activation('relu')(X)
+    X = MaxPooling2D((3, 3), strides=(2, 2))(X)
+
+    X = convolutional_block(X, f=3, filters=[64, 64, 256], stage=2, block='a', s=1)
+    X = identity_block(X, 3, [64, 64, 256], stage=2, block='b')
+    X = identity_block(X, 3, [64, 64, 256], stage=2, block='c')
+
+
+    X = convolutional_block(X, f=3, filters=[128, 128, 512], stage=3, block='a', s=2)
+    X = identity_block(X, 3, [128, 128, 512], stage=3, block='b')
+    X = identity_block(X, 3, [128, 128, 512], stage=3, block='c')
+    X = identity_block(X, 3, [128, 128, 512], stage=3, block='d')
+
+    X = convolutional_block(X, f=3, filters=[256, 256, 1024], stage=4, block='a', s=2)
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='b')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='c')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='d')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='e')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='f')
+
+    X = X = convolutional_block(X, f=3, filters=[512, 512, 2048], stage=5, block='a', s=2)
+    X = identity_block(X, 3, [512, 512, 2048], stage=5, block='b')
+    X = identity_block(X, 3, [512, 512, 2048], stage=5, block='c')
+
+    X = AveragePooling2D(pool_size=(2, 2), padding='same')(X)
+    
+    model = Model(inputs=X_input, outputs=X, name='ResNet50')
+
+    return model
+
+
+base_model = ResNet50(input_shape=(256, 256, 3))
+headModel = base_model.output
+headModel = Flatten()(headModel)
+headModel=Dense(256, activation='relu', name='fc1',kernel_initializer=glorot_uniform(seed=0))(headModel)
+headModel=Dense(128, activation='relu', name='fc2',kernel_initializer=glorot_uniform(seed=0))(headModel)
+headModel = Dense(9,activation='softmax', name='fc3',kernel_initializer=glorot_uniform(seed=0))(headModel)
+
+
+
+model = Model(inputs=base_model.input, outputs=headModel)
+model.summary()
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+x_test = np.load('DATA/Original/x_test.npy')
+y_test = np.load('DATA/Original/y_test.npy')
+x_train = np.load('DATA/Original/x_train.npy')
+y_train = np.load('DATA/Original/y_train.npy')
+history = model.fit(x_train, y_train, batch_size=50, epochs=200, verbose=1, validation_split=0.2)    
+test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
+
+    
+model = Model(inputs=base_model.input, outputs=headModel)
+model.summary()
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+x_train = np.load('DATA/Generic/x_train.npy')
+y_train = np.load('DATA/Generic/y_train.npy')
+history = model.fit(x_train, y_train, batch_size=50, epochs=200, verbose=1, validation_split=0.2)    
+test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
+
+
+model = Model(inputs=base_model.input, outputs=headModel)
+model.summary()
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+x_train = np.load('DATA/Our_method/x_train.npy')
+y_train = np.load('DATA/Our_method/y_train.npy')   
+history = model.fit(x_train, y_train, batch_size=50, epochs=200, verbose=1, validation_split=0.2)    
+test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
